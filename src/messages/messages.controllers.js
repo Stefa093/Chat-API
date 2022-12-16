ba928c3374d8297691e67a853830f1c22e792ee3
Stefa093 <estefaniasalazar093@gmail.com>
2022-12-16 11:40:44 -0500

    find all messages

--- src/messages/messages.controllers.js
+++ src/messages/messages.controllers.js
@@ -1,10 +1,20 @@
 const Messages = require('../models/messages.models')
+const Conversations = require('../models/conversations.models')
 const uuid = require('uuid')
+
+const findAllMessages = async () => {
+    const data = await Messages.findAll({
+        include: {
+            model : Conversations
+        }
+    })
+    return data
+}
 
 const createMessage = async (obj) => {
     const data = await Messages.create({
         id: uuid.v4(),
-        userId: object.userId,
+        userId: obj.userId,
         conversationId: obj.conversationId,
         message: obj.message
     })
@@ -12,5 +22,6 @@ const createMessage = async (obj) => {
 }
 
 module.exports = {
+    findAllMessages,
     createMessage
 }
